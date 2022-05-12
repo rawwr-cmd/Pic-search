d21cde264d03b2f405972baf5d385dec21760ed9
rawwr <akshayace0302@gmail.com>
2022-05-12 21:27:17 +0530

    implementing keys and destructuring

--- src/Components/ImageList.js
+++ src/Components/ImageList.js
@@ -2,8 +2,9 @@ import React from "react";
 
 const ImageList = (props) => {
   // console.log(props.images);
-  const images = props.images.map(({ urls }) => {
-    return <img src={urls.regular} alt="images.urls.regular" />;
+  const images = props.images.map(({ urls, id, description }) => {
+    //we assign the key to the root element
+    return <img key={id} src={urls.regular} alt={description} />;
   });
 
   return <div>{images}</div>;
