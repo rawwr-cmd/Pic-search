16de6350d5a0ceb351b06bb9f6bcf1dd077e411e
rawwr <akshayace0302@gmail.com>
2022-05-12 23:49:46 +0530

    imageCard and styling

--- src/Components/ImageList.js
+++ src/Components/ImageList.js
@@ -1,13 +1,15 @@
+import "../Styles/ImageList.css";
+import ImageCard from "./ImageCard";
 import React from "react";
 
 const ImageList = (props) => {
   // console.log(props.images);
-  const images = props.images.map(({ urls, id, description }) => {
+  const images = props.images.map((image) => {
     //we assign the key to the root element
-    return <img key={id} src={urls.regular} alt={description} />;
+    return <ImageCard key={image.id} image={image} />;
   });
 
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
