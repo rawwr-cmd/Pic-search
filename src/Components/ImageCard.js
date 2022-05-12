9a9e372ad237fd6c26a648885ebd6e36ba9e9624
rawwr <akshayace0302@gmail.com>
2022-05-13 00:15:39 +0530

    callback on image load

--- src/Components/ImageCard.js
+++ src/Components/ImageCard.js
@@ -1,11 +1,26 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.imageRef = React.createRef();
+  }
+
+  //you can directly replace this.setSpans with an arrow functions
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.setSpans);
+  }
+
+  setSpans = () => {
+    console.log(this.imageRef.current.clientHeight);
+  };
+
   render() {
     const { description, urls } = this.props.image;
     return (
       <div>
-        <img alt={description} src={urls.regular} />
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
   }
